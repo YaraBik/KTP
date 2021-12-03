468041bc057be9db2fc27e4a69c2b5b148b141d4
Farrukh Baratov <farrukhsbaratov@gmail.com>
2021-12-03 10:31:41 +0100

    answers saved to answers.txt, question displayed above textbox

--- main.py
+++ main.py
@@ -3,23 +3,7 @@
 # output depend on if yes/no is true
 
 import wx
-
-
-def main():
-    print("Wat is reason that you decided to go to the counsellor?")
-    input_msg = "answer with 'yes' or 'no': "
-    while True:
-        ans = input(input_msg)
-        ans = ans.lower()
-        if ans == 'yes':
-            print("Correct answer!")
-            break
-        elif ans == 'no':
-            print("Wrong answer!")
-            input_msg = "answer with 'yes': "
-        else:
-            print("Invalid input!")
-
+from datetime import datetime
 
 class MyPanel(wx.Panel):
     frame = False
@@ -28,20 +12,39 @@ class MyPanel(wx.Panel):
         super().__init__(parent)
         frame = parent
 
-        button1 = wx.Button(self, label='Yes')
+        button1 = wx.Button(self, label='Submit answer')
         button1.Bind(wx.EVT_BUTTON, self.on_button1)
 
-        button2 = wx.Button(self, label='No')
+        button2 = wx.Button(self, label='Quit application')
         button2.Bind(wx.EVT_BUTTON, self.on_button2)
 
-        main_sizer = wx.BoxSizer(wx.HORIZONTAL)
-        main_sizer.Add(button1, proportion=1,
-                       flag=wx.ALL | wx.CENTER | wx.EXPAND,
+        q1 = wx.StaticText(self, label="What is reason that you decided to go to the counsellor?")
+
+        self.text1 = wx.TextCtrl(self, -1, size=(175, 50))
+
+        main_sizer = wx.BoxSizer(wx.VERTICAL)
+        main_sizer.Add(q1, proportion=1,
+                       flag=wx.ALL | wx.BOTTOM,
                        border=5)
-        main_sizer.Add(button2, 0, wx.ALL, 5)
+        main_sizer.Add(self.text1, proportion=1,
+                       flag=wx.EXPAND,
+                       border=5)
+        main_sizer.Add(button1, proportion=1,
+                       flag=wx.EXPAND | wx.ALL,
+                       border=5)
+        main_sizer.Add(button2, proportion=1,
+                       flag=wx.EXPAND | wx.ALL,
+                       border=5)
+
         self.SetSizer(main_sizer)
 
     def on_button1(self, event):
+        answer = self.text1.GetValue()
+        with open('answers.txt', 'a') as f:
+            now = datetime.now()
+            f.write(now.strftime("%d-%m-%Y %H:%M:%S") + ': ')
+            f.write(answer)
+            f.write('\n')
         frame.Close()
 
     def on_button2(self, event):
