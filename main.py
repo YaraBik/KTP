b25b6b0a3804bc272fcd97dd016e4a51d56b2d47
YaraBik <bikowski24@gmail.com>
2021-12-08 14:34:36 +0100

    more questions

--- main.py
+++ main.py
@@ -3,63 +3,11 @@
 # output depend on if yes/no is true
 
 import wx
-from datetime import datetime
-
-class MyPanel(wx.Panel):
-    frame = False
-
-    def __init__(self, parent):
-        super().__init__(parent)
-        frame = parent
-
-        button1 = wx.Button(self, label='Submit answer')
-        button1.Bind(wx.EVT_BUTTON, self.on_button1)
-
-        button2 = wx.Button(self, label='Quit application')
-        button2.Bind(wx.EVT_BUTTON, self.on_button2)
-
-        q1 = wx.StaticText(self, label="What is reason that you decided to go to the counsellor?")
-
-        self.text1 = wx.TextCtrl(self, -1, size=(175, 50))
-
-        main_sizer = wx.BoxSizer(wx.VERTICAL)
-        main_sizer.Add(q1, proportion=1,
-                       flag=wx.ALL | wx.BOTTOM,
-                       border=5)
-        main_sizer.Add(self.text1, proportion=1,
-                       flag=wx.EXPAND,
-                       border=5)
-        main_sizer.Add(button1, proportion=1,
-                       flag=wx.EXPAND | wx.ALL,
-                       border=5)
-        main_sizer.Add(button2, proportion=1,
-                       flag=wx.EXPAND | wx.ALL,
-                       border=5)
-
-        self.SetSizer(main_sizer)
-
-    def on_button1(self, event):
-        answer = self.text1.GetValue()
-        with open('answers.txt', 'a') as f:
-            now = datetime.now()
-            f.write(now.strftime("%d-%m-%Y %H:%M:%S") + ': ')
-            f.write(answer)
-            f.write('\n')
-        frame.Close()
-
-    def on_button2(self, event):
-        frame.Close()
-
-
-class MyFrame(wx.Frame):
-
-    def __init__(self):
-        super().__init__(None, title='Hello World')
-        panel = MyPanel(self)
-        self.Show()
+from openQuestions import OpenQFrame
 
 
 if __name__ == '__main__':
+    questions = {}
     app = wx.App(redirect=False)
-    frame = MyFrame()
+    frame = OpenQFrame()
     app.MainLoop()
