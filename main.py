dff5ea2ca0f34bc7896d9f755cc3b68f390c4fe4
YaraBik <bikowski24@gmail.com>
2021-12-09 22:50:08 +0100

    switch between two multiple choice panels

--- main.py
+++ main.py
@@ -4,6 +4,8 @@
 
 import wx
 import openQuestions
+import multipleChoice
+from rangeQuestions import testFrame
 
 
 class Program(wx.Frame):
@@ -11,31 +13,49 @@ class Program(wx.Frame):
     question_number = 0
 
     def __init__(self):
-        # wx.Frame.__init__(self, None, wx.ID_ANY, 'Program')
+        super().__init__(None, title='Goodbye World :] :>')
+        # p1 = multipleChoice.MultipleChoicePanel(self, "Question 1")
 
-        # panel_one = Glowne(self)
-        # self.panel_two = Glowne1(self)
-        # self.panel_two.Hide()
+        sizer = wx.BoxSizer()
+        self.SetSizer(sizer)
+
+        self.panel_one = multipleChoice.MultipleChoicePanel(self, "Question 1")
+        sizer.Add(self.panel_one, 1, wx.EXPAND)
+        self.panel_one.btn.Bind(wx.EVT_BUTTON, self.show_panel_two)
+        self.panel_two = multipleChoice.MultipleChoicePanel(self, "Question 2")
+        sizer.Add(self.panel_two, 1, wx.EXPAND)
+        self.panel_two.btn.Bind(wx.EVT_BUTTON, self.show_panel_one)
+        self.panel_two.Hide()
+        self.SetSize((1000, 800))
+        self.Centre()
+
+    def show_panel_one(self, event):
+        self.panel_one.Show()
+        self.panel_two.Hide()
+        self.Layout()
+
+    def show_panel_two(self, event):
+        self.panel_two.Show()
+        self.panel_one.Hide()
+        self.Layout()
+
+        # self.panels.append(openQuestions.OpenQPanel(self, "Introduction!"))
+        # self.panels[self.question_number].Show()
+        #
+        # qs = ["Question 1: What is reason that you decided to go to the counsellor?", "Question 2"]
+        # for i in range(len(qs)):
+        #     q = qs[i]
+        #     self.panels.append(openQuestions.OpenQPanel(self, q))
+        #     self.panels[i + 1].Hide()
+        #
         # self.SetSize((800, 600))
         # self.Centre()
-
-        super().__init__(None, title='Goodbye World :] :>')
-        self.panels.append(openQuestions.OpenQPanel(self, "Introduction!"))
-        self.panels[self.question_number].Show()
-
-        qs = ["Question 1: What is reason that you decided to go to the counsellor?", "Question 2"]
-        for i in range(len(qs)):
-            q = qs[i]
-            self.panels.append(openQuestions.OpenQPanel(self, q))
-            self.panels[i + 1].Hide()
-
-        self.SetSize((800, 600))
-        self.Centre()
 
 
 if __name__ == '__main__':
     questions = {}
     app = wx.App(redirect=False)
+    # frame = testFrame()
     frame = Program()
     frame.Show()
     app.MainLoop()
